919c490365d4aca3472a3251c60baaf15eb20b18
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-08-24 11:53:22 -0700

    Create ApeSample.sol
    create sample erc20 file to see if we can pay for mint with it. used for testing

---
+++ packages/hardhat/contracts/ApeSample.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ApeSample is ERC20 {
+    constructor() ERC20("ApeSamp", "Sape") {
+        _mint(msg.sender, 1000 ether); // mints 1000 balloons!
+    }
+}
