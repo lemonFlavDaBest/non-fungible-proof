070816011d86ac145461323d66b612602103b298
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-09-06 10:49:43 -0700

    Update App.jsx
    spacing

--- packages/react-app/src/App.jsx
+++ packages/react-app/src/App.jsx
@@ -24,7 +24,7 @@ import {
   FaucetHint,
   NetworkSwitch,
 } from "./components";
-import { NETWORKS, ALCHEMY_KEY } from "./constants";
+import { NETWORKS, ALCHEMY_KEY} from "./constants";
 import externalContracts from "./contracts/external_contracts";
 // contracts
 import deployedContracts from "./contracts/hardhat_contracts.json";
