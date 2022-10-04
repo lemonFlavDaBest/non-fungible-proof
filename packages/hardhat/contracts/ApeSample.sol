92d877b514421616848f3256f6c733bd9c664d51
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-10-04 16:12:29 -0700

    Update ApeSample.sol
    adding my address to the constructor so shizz is sent straight tummy

--- packages/hardhat/contracts/ApeSample.sol
+++ packages/hardhat/contracts/ApeSample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ApeSample is ERC20 {
-    constructor() ERC20("ApeSamp", "Sape") {
-        _mint(msg.sender, 1000 ether); // mints 1000 ApeSample tokens!
+    constructor(address my_addr) ERC20("ApeSamp", "Sape") {
+        _mint(my_addr, 1000 ether); // mints 1000 ApeSample tokens!
     }
 }
