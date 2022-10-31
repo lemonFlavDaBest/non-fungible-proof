afb3d1dfe86c21d4044cec2ab5adfada0bb1bee1
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-10-31 15:44:39 -0700

    Update myTest.js
    messing with testing

--- packages/hardhat/test/myTest.js
+++ packages/hardhat/test/myTest.js
@@ -39,3 +39,48 @@ describe("My Dapp", function () {
     });
   });
 });
+
+describe("My Dapp", function () {
+  let proofContract;
+  let burnContract;
+  let nftContract;
+  let ercContract;
+
+  // quick fix to let gas reporter fetch data from gas station & coinmarketcap
+  before((done) => {
+    setTimeout(done, 2000);
+  });
+
+  describe("deploy all contracts", function () {
+    
+    it("Should deploy all contracts", async function () {
+      const NFProof = await ethers.getContractFactory("NFProof");
+      proofContract = await NFProof.deploy();
+      const TheBurn = await ethers.getContractFactory("TheBurn");
+      burnContract = await TheBurn.deploy(proofContract.address)
+      const SampleNFT = await ethers.getContractFactory("SampleNFT");
+      nftContract = await SampleNFT.deploy()
+      const ApeSample = await ethers.getContractFactory("ApeSample")
+      ercContract = await ApeSample.deploy("0x13D029DbB5fc28A9D9450B18442879FDb87E901e")
+    });
+
+    describe("mint()", function () {
+      it("Should be able to mint from token you own", async function () {
+        const newPurpose = "Test Purpose";
+
+        await myContract.setPurpose(newPurpose);
+        expect(await myContract.purpose()).to.equal(newPurpose);
+      });
+
+      it("Should emit a SetPurpose event ", async function () {
+        const [owner] = await ethers.getSigners();
+
+        const newPurpose = "Another Test Purpose";
+
+        expect(await myContract.setPurpose(newPurpose))
+          .to.emit(myContract, "SetPurpose")
+          .withArgs(owner.address, newPurpose);
+      });
+    });
+  });
+});
