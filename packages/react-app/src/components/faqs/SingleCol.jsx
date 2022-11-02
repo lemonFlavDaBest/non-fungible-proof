475868087cff3e80f38c0e05f417ca82f82fc928
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-11-02 14:28:55 -0700

    change to jsx
    change file to jsx

--- packages/react-app/src/components/faqs/SingleCol.jsx
+++ packages/react-app/src/components/faqs/SingleCol.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { motion } from "framer-motion";
+//import { motion } from "framer-motion";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { css } from "styled-components/macro"; //eslint-disable-line
-import { SectionHeading, Subheading as SubheadingBase } from "components/misc/Headings.js";
-import { SectionDescription } from "components/misc/Typography.js";
-import { Container, ContentWithPaddingXl } from "components/misc/Layouts.js";
+import { SectionHeading, Subheading as SubheadingBase } from "../misc/Headings.js";
+import { SectionDescription } from "../misc/Typography.js";
+import { Container, ContentWithPaddingXl } from "../misc/Layouts.js";
 import { ReactComponent as ChevronDownIcon } from "feather-icons/dist/icons/chevron-down.svg";
-import { ReactComponent as SvgDecoratorBlob1 } from "images/svg-decorator-blob-7.svg";
-import { ReactComponent as SvgDecoratorBlob2 } from "images/svg-decorator-blob-8.svg";
+import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-7.svg";
+import { ReactComponent as SvgDecoratorBlob2 } from "../../images/svg-decorator-blob-8.svg";
 
 const Subheading = tw(SubheadingBase)`mb-4 text-center`;
 const Heading = tw(SectionHeading)`w-full`;
@@ -21,13 +21,13 @@ const FAQSContainer = tw.dl`mt-12 max-w-4xl relative`;
 const FAQ = tw.div`cursor-pointer select-none mt-5 px-8 sm:px-10 py-5 sm:py-4 rounded-lg text-gray-800 hover:text-gray-900 bg-gray-200 hover:bg-gray-300 transition duration-300`;
 const Question = tw.dt`flex justify-between items-center`;
 const QuestionText = tw.span`text-lg lg:text-xl font-semibold`;
-const QuestionToggleIcon = motion(styled.span`
+const QuestionToggleIcon = styled.span`
   ${tw`ml-2 transition duration-300`}
   svg {
     ${tw`w-6 h-6`}
   }
-`);
-const Answer = motion(tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`);
+`
+const Answer = tw.dd`pointer-events-none text-sm sm:text-base leading-relaxed`
 
 const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
   ${tw`pointer-events-none -z-20 absolute right-0 top-0 h-56 w-56 opacity-15 transform translate-x-2/3 -translate-y-12 text-teal-400`}
@@ -38,33 +38,33 @@ const DecoratorBlob2 = styled(SvgDecoratorBlob2)`
 
 
 
-export default ({
+export function SingleCol ({
   subheading = "FAQS",
-  heading = "You have Questions ?",
-  description = "And we have got answers to all of them. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  heading = "You have Questions?",
+  description = "And we have got answers to all of them.",
   faqs = [
     {
-      question: "Is lunch provided free of cost ?",
+      question: "Will I lose my NFT ?",
       answer:
-        "Yes, it is, if you have a membership with us. Otherwise it is charged as per the menu. Some limits do apply as to how much items can be included in your lunch. This limit is enough for any one person and merely exists to discourage abusal of the system."
+        "There is always risk, but probably not. The contract does not interact with or give permissions for your NFT. It only checks if you own it."
     },
     {
-      question: "Do you have 2 Bedroom suites ?",
+      question: "What happens to the NFP Token if the original NFT is sold or transferred?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      "An NFP token proves ownership over an NFT. If that NFT is ever transferred or sold, the NFP token will be marked as invalid and can be burned by the real owner. "
     },
     {
-      question: "Are Wi-Fi costs included in the price ?",
+      question: "Can I implement this with my project ?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+      "Yes and it is so so easy. A couple lines of code (we'll give you), and it's ready for your next event or airdrop. LFB!"
     },
     {
-      question: "Where can I reach you for support ?",
+      question: "Is it free ?",
       answer:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-    }
+        "Unfortunately, no. However all you need to pay is a small mint fee (.001 ETH or 1 $APE). "
+    },
   ]
-}) => {
+}) {
   const [activeQuestionIndex, setActiveQuestionIndex] = useState(null);
 
   const toggleQuestion = questionIndex => {
