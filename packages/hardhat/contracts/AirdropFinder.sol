5d1a7c829023deaa50d09f82b3c8fc0f86b21cf1
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-12-06 10:42:24 -0800

    Update AirdropFinder.sol
    change name. add function logic

--- packages/hardhat/contracts/AirdropFinder.sol
+++ packages/hardhat/contracts/AirdropFinder.sol
@@ -4,29 +4,26 @@ pragma solidity ^0.8.0;
 import "./NFProof.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WalletFinder is Ownable {
+contract AirdropFinder is Ownable {
 
     //the purpose of this contract is for developers to use it find valid wallet addresses for airdrops and similar things.
     //if a valid NFP token is assigned, then it will return that address, otherwise it will return the underlying owner of the nft
 
-    uint256 public burnPrice;
     NFProof public nfProof;
 
     constructor(address payable proofContract) {
-      burnPrice = 1000000000; //.000000001 ether
       nfProof = NFProof(proofContract);
     }
 
-   function setburnPrice(uint256 newPrice) public onlyOwner {
-      burnPrice = newPrice;
-    }
- 
     // this function returns an address. you can use this address to send airdrops to. It will check if there is a valid NFP token owner assigned
     // and if there isnt, it defaults to the underlying nft owner. You can use it for things other than airdrops, any time you need to get a valid wallet address.
-    function airdropFinder(address originContractAddress, uint256 originTokenId, uint256 proofTokenId) external payable virtual {
-      require(msg.value>=burnPrice, "you didnt pay enough to the burn troll");
-      require(IERC721(originContractAddress).ownerOf(originTokenId) == msg.sender, "You do not own this NFT");
-      nfProof.burn(originContractAddress, originTokenId, proofTokenId);
+    function airdropFinder(address originContractAddress, uint256 originTokenId) external view virtual returns (address airdropAddress) {
+      uint256 proofToken = nfProof.tokenToToken(originContractAddress, originTokenId);
+      if (nfProof.isValidUserToken(proofToken) == true) {
+        return nfProof.userOf(proofToken);
+      } else {
+        return IERC721(originContractAddress).ownerOf(originTokenId);
+      }
     }
 
     function walletFinderWithdraw() public onlyOwner {
