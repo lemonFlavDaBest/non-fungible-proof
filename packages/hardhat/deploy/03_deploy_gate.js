a7c2270b6fce9a1f6951ccdb573bd9a6fb5c3ed4
jclare93 <106985713+lemonFlavDaBest@users.noreply.github.com>
2022-12-06 10:48:04 -0800

    deployment scripts
    add the deployment scripts

---
+++ packages/hardhat/deploy/03_deploy_gate.js
@@ -0,0 +1,77 @@
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+
+  console.log("deployer is:", deployer)
+
+  console.log('deploying additional contracts')
+  const nfProof = await ethers.getContract("NFProof", deployer);
+  console.log("nfProof", nfProof)
+
+  await deploy("TokenGate", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args:[nfProof.address],
+    log: true,
+  });
+
+  sleep(5000)
+  
+
+  const tokenGate = await ethers.getContract("TokenGate", deployer);
+  sleep(5000)
+  
+  //are these the ones we wnat to transfer ownship too?
+  const tokenGateOwnership = await tokenGate.transferOwnership("0x13D029DbB5fc28A9D9450B18442879FDb87E901e");
+  const tokenGateResult = await tokenGateOwnership.wait()
+  console.log(tokenGateResult)
+
+  sleep(5000)
+
+  await deploy("AirdropFinder", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args:[nfProof.address],
+    log: true,
+  });
+
+  sleep(5000)
+  
+
+  const airdropFinder = await ethers.getContract("AirdropFinder", deployer);
+  sleep(5000)
+  
+  //are these the ones we wnat to transfer ownship too?
+  const airdropFinderOwnership = await airdropFinder.transferOwnership("0x13D029DbB5fc28A9D9450B18442879FDb87E901e");
+  const airdropFinderResult = await airdropFinderOwnership.wait()
+  console.log(airdropFinderResult)
+
+  sleep(5000)
+
+
+console.log("DEPLOYEDADD");
+
+/*
+console.log("post deploy")
+await deploy("SampleNFT", {
+  // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+  from: deployer,
+  log: true,
+});
+
+console.log("post deploy finished")
+*/
+  
+};
+module.exports.tags = ["TokenGate"];
---
+++ packages/hardhat/deploy/04_deploy_finder.js
@@ -0,0 +1,49 @@
+const { ethers } = require("hardhat");
+
+const localChainId = "31337";
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+
+module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = await getChainId();
+
+
+  console.log("deployer is:", deployer)
+
+  console.log('deploying additional contracts')
+  const nfProof = await ethers.getContract("NFProof", deployer);
+  console.log("nfProof", nfProof)
+
+  sleep(5000)
+
+  await deploy("AirdropFinder", {
+    // Learn more about args here: https://www.npmjs.com/package/hardhat-deploy#deploymentsdeploy
+    from: deployer,
+    args:[nfProof.address],
+    log: true,
+  });
+
+  sleep(5000)
+  
+
+  const airdropFinder = await ethers.getContract("AirdropFinder", deployer);
+  sleep(5000)
+  
+  //are these the ones we wnat to transfer ownship too?
+  const airdropFinderOwnership = await airdropFinder.transferOwnership("0x13D029DbB5fc28A9D9450B18442879FDb87E901e");
+  const airdropFinderResult = await airdropFinderOwnership.wait()
+  console.log(airdropFinderResult)
+
+  sleep(5000)
+
+
+console.log("DEPLOYEDADD");
+
+
+};
+module.exports.tags = ["Finder"];
